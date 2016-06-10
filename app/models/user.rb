857a80ed5c224ef794af364824f0cf235e6ad3df
Eric Lacaille <elacaille0@gmail.com>
2016-06-10 13:35:25 +0200

    solved team display

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.team_jouve
+    User.where("jouve = ?", true)
+    # .map { |member| "#{member.first_name} #{member.last_name}"}
+  end
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
