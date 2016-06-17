e3084e75896ea55555bf268470907f65521565ff
Eric Lacaille <elacaille0@gmail.com>
2016-06-17 18:41:47 +0200

    tabs of appointment completed

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,7 @@
 class Appointment < ActiveRecord::Base
   belongs_to :candidate
+
+  def interviewer_full_name
+    "#{self.interviewer_first_name.capitalize} #{self.interviewer_last_name}"
+  end
 end
