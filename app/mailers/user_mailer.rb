ac76ea96f34dccbdab2d8fe7724eb63b135f6dbd
Eric Lacaille <elacaille0@gmail.com>
2016-06-27 17:29:03 +0200

    gestion password reset under postmark - move view mailer devise under users mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-
+  include Devise::Mailers::Helpers
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -10,4 +10,26 @@ class UserMailer < ApplicationMailer
     @password = password
     mail(to: @user.email, subject:"Your login to Jouveo - Jouve & Associés" )
   end
+
+  def confirmation_instructions(record, token, opts={})
+    @token = token
+    devise_mail(record, :confirmation_instructions, opts)
+  end
+
+  def reset_password_instructions(record, token, opts={})
+    @token = token
+    devise_mail(record, :reset_password_instructions, opts)
+  end
+
+  def unlock_instructions(record, token, opts={})
+    @token = token
+    devise_mail(record, :unlock_instructions, opts)
+  end
+
+  # def reset_password_instructions(user, token, options)
+  #   @resource = user
+  #   mail(:to => @resource.email, :subject => "Reset password instructions", :tag => 'password-reset', :content_type => "text/html") do |format|
+  #     format.html { render "devise/mailer/reset_password_instructions" }
+  #   end
+  # end
 end
