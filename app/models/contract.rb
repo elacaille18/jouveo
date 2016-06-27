c104d22a1ac6037b5a6b845c36ebc71380b322e2
Eric Lacaille <elacaille0@gmail.com>
2016-06-27 10:14:46 +0200

    deleted files of models

--- app/models/contract.rb
+++
@@ -1,5 +0,0 @@
-class Contract < ActiveRecord::Base
-  # Associations
-  belongs_to :company
-  belongs_to :candidate
-end
--- app/models/school.rb
+++
@@ -1,6 +0,0 @@
-class School < ActiveRecord::Base
-  # Associations
-  has_many :diplomas
-  has_many :candidates, through: :diplomas
-
-end
