bd9711d96753150c242ea0c223cf9586e16577a7
Eric Lacaille <elacaille0@gmail.com>
2016-06-29 17:38:38 +0200

    solved appoitment form issue

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,9 +4,9 @@ class Appointment < ActiveRecord::Base
   # Validations
   validates :candidate, presence: true
   validates :begins_at, presence: true
-  validates :interviewer_first_name, presence: true
-  validates :interviewer_last_name, presence: true
-  validates :interviewer_position, presence: true
+  # validates :interviewer_first_name, presence: true
+  # validates :interviewer_last_name, presence: true
+  # validates :interviewer_position, presence: true
 
   def interviewer_full_name
     "#{self.interviewer_first_name.capitalize} #{self.interviewer_last_name}"
